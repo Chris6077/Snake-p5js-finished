d81dd2133dd98b6e50cbd651894cf5d40243c177
TacticalIdiot <realgoldencoldlp@gmail.com>
2017-01-02 06:49:44 +0100

    Added Game

---
+++ engine.js
@@ -0,0 +1,100 @@
+﻿var s;  //snake
+var scl = 20;   //scale of grid
+var food;   //food
+var frameStep = 10; //frameRate (speed of Snake)
+
+function setup() {
+    //unbinding keys with scroll function
+    //(disabling the scroll function of the keyboard)
+    var keys = {};
+    window.addEventListener("keydown",
+        function (e) {
+            keys[e.keyCode] = true;
+            switch (e.keyCode) {
+                case 37: case 39: case 38: case 40: //Arrow keys
+                case 32: e.preventDefault(); break; //Space
+                default: break; //do not block other keys
+            }
+        },
+    false);
+    window.addEventListener('keyup',
+        function (e) {
+            keys[e.keyCode] = false;
+        },
+    false);
+    //unbinding keys with other functions
+    $(document).unbind("keypress.key37");
+    $(document).unbind("keypress.key38");
+    $(document).unbind("keypress.key39");
+    $(document).unbind("keypress.key40");
+    createCanvas(600, 600);
+    s = new Snake();
+    //making snake slower
+    frameRate(frameStep);
+    pickLocation();
+    //setting start scores
+    document.getElementById("score").innerHTML = ("Last Score: " + 0 + " --- " + "Highscore: " + (localStorage['highscore'] || 0));
+}
+
+function pickLocation() {
+    var cols = floor(width / scl);
+    var rows = floor(height / scl);
+    var rndmx;
+    var rndmy;
+    do {    // blocking food spawn on snake
+        rndmx = floor(random(cols));
+        rndmy = floor(random(rows));
+    } while (s.isOnSnake(rndmx, rndmy));
+    //creating food (not drawing)
+    food = createVector(rndmx, rndmy);
+    food.mult(scl);
+}
+
+//bad idea -> what if the snake eats? not good for testing
+/*function mousePressed() {
+    s.total++;
+}*/
+
+function draw() {
+    background(51);
+    if (s.eat(food)) {  //respawning food if snake got it
+        pickLocation();
+    }
+    s.death();
+    s.update();
+    s.show();
+    //coloring food
+    fill(255, 0, 100);
+    //drawing food
+    rect(food.x, food.y, scl, scl);
+}
+
+function keyPressed() {
+    if (frameCount > s.getNextPossibleFrame()) {    //blocking multiple actions (turning) in one step
+        if (keyCode === UP_ARROW) {
+            if (s.getLastDirection() == 37 || s.getLastDirection() == 39) {
+                s.setLastDirection(38);
+                s.dir(0, -1);
+                s.setNextPossibleFrame(frameCount); //if pressed key is invalid user can press another one so we need this in each if and not at the end
+            }
+        } else if (keyCode === DOWN_ARROW) {
+            if (s.getLastDirection() == 37 || s.getLastDirection() == 39) {
+                s.setLastDirection(40);
+                s.dir(0, 1);
+                s.setNextPossibleFrame(frameCount); //if pressed key is invalid user can press another one so we need this in each if and not at the end
+            }
+        } else if (keyCode === RIGHT_ARROW) {
+            if (s.getLastDirection() == 38 || s.getLastDirection() == 40) {
+                s.setLastDirection(39);
+                s.dir(1, 0);
+                s.setNextPossibleFrame(frameCount); //if pressed key is invalid user can press another one so we need this in each if and not at the end
+            }
+        } else if (keyCode === LEFT_ARROW) {
+            if (s.getLastDirection() == 38 || s.getLastDirection() == 40) {
+                s.setLastDirection(37);
+                s.dir(-1, 0);
+                s.setNextPossibleFrame(frameCount); //if pressed key is invalid user can press another one so we need this in each if and not at the end
+            }
+        }
+    }
+}
---
+++ snake.js
@@ -0,0 +1,119 @@
+﻿function Snake() {
+    this.step = 20; //Grid size
+    this.x = this.step; //x position of head
+    this.y = this.step; //y position of head
+    this.lastx = this.step; //last x position of head
+    this.lasty = this.step; //last y position of head
+    this.xspeed = 1; //x speed for grid
+    this.yspeed = 0; //y speed for grid
+    this.total = 0; //total food gained
+    this.tail = []; //tail of snake
+    this.lastdir = 39; //last key pressed (for blocking opposite direction)
+    this.window = 600; //window size
+    this.nextPossibleFrame = 0; //next possible action (frameCount) for blocking opposite direction (multiple moves in one frame)
+    this.highscore = localStorage['highscore'] || 0; //cached high score or 0 if not set
+
+    this.getLastDirection = function() {
+        return this.lastdir;
+    }
+
+    this.setLastDirection = function (last) {
+        this.lastdir = last;
+    }
+
+    this.setNextPossibleFrame = function (next) {
+        this.nextPossibleFrame = next;
+    }
+
+    this.getNextPossibleFrame = function () {
+        return this.nextPossibleFrame;
+    }
+
+    this.eat = function (pos) { //checks if snake is able to eat the food
+        var d = dist(this.x, this.y, pos.x, pos.y);
+        if (d < 1) {
+            this.total++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    this.dir = function (x, y) {
+        this.xspeed = x;
+        this.yspeed = y;
+    }
+
+    this.death = function () {   //calls reset on colission with tail or wall
+        for (var i = 0; i < this.tail.length; i++) {
+            var pos = this.tail[i];
+            d = dist(this.x, this.y, pos.x, pos.y);
+            if (d < 1) this.reset();
+        }
+        if (
+            (this.x <= 0 && this.lastx <= 0 && this.lasty == this.y)
+            ||
+            (this.x >= (this.window - this.step) && this.lastx >= (this.window - this.step) && this.lasty == this.y)
+            ||
+            (this.y <= 0 && this.lasty <= 0 && this.lastx == this.x)
+            ||
+            (this.y >= (this.window - this.step) && this.lasty >= (this.window - this.step) && this.lastx == this.x)
+        ) this.reset();
+    }
+
+    this.isOnSnake = function (x, y) {  //checks if coord is on snake
+        var res = false;
+        if (this.tail.length > 0) {
+            var idx = 0;
+            while (idx < this.tail.length && !res) {
+                var pos = this.tail[idx];
+                if (pos.x == x && pos.y == y) res = true;
+                idx++;
+            }
+        }
+        else if (this.x == x && this.y == y) res = true;
+        return res;
+    }
+
+    this.reset = function () { //sets snake to start position and manages scores
+        if (this.highscore < this.total) {  //setting cached highscore if broken
+            this.highscore = this.total;
+            localStorage['highscore'] = "" + this.highscore;
+        }
+        //showing scores
+        document.getElementById("score").innerHTML = ("Last Score: " + this.total + " --- " + "Highscore: " + localStorage['highscore']);
+        //setting snake to start position
+        this.x = this.step;
+        this.y = this.step;
+        this.lastx = this.step;
+        this.lasty = this.step;
+        this.xspeed = 1;
+        this.yspeed = 0;
+        this.lastdir = 39;
+        this.total = 0;
+        this.tail = [];
+    }
+
+    this.update = function () { //moving snake
+        if (this.total === this.tail.length) {
+            for (var i = 0; i < this.tail.length - 1; i++) {
+                this.tail[i] = this.tail[i + 1];
+            }
+        }
+        this.tail[this.total - 1] = createVector(this.x, this.y);
+        this.lastx = this.x;
+        this.lasty = this.y;
+        this.x = this.x + this.xspeed * scl;
+        this.y = this.y + this.yspeed * scl;
+        this.x = constrain(this.x, 0, width - scl);
+        this.y = constrain(this.y, 0, height - scl);
+    }
+
+    this.show = function () {   //drawing snake
+        fill(255);
+        for (var i = 0; i < this.tail.length; i++) {
+            rect(this.tail[i].x, this.tail[i].y, scl, scl);
+        }
+        rect(this.x, this.y, scl, scl);
+    }
+}
